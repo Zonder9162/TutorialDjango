7d33ff12f3c04bb535960ef9a530a47456fe50f6
Zonder9162 <zonder9162@gmail.com>
2022-07-31 21:54:49 +0600

    tutorialdjango

---
+++ snippets/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from snippets import views
+
+urlpatterns = [
+    path('snippets/', views.snippet_list),
+    path('snippets/<int:pk>/', views.snippet_detail),
+]
+
+from django.urls import path, include
+
+urlpatterns = [
+    path('', include('snippets.urls')),
+]
